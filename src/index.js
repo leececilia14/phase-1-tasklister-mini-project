8db5cd1b7811b51ad18fea3b13a840a34f755e2c
Cecilia Lee <leececilia14@gmail.com>
2021-04-12 10:32:38 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasklist= new Tasklist();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const taskUl = document.getElementById("tasks");
+
+newTaskForm.addEventListener("submit", createNewTask);
+});
+
+const createNewTask= event => {
+  event.preventDefault(); 
+
+  const newTaskDescription=document.getElementById("new-task-description");
+  const newTask=document.createElement("li");
+  newTask.innerText= newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.targegt.reset(); 
+};
+
+const appendNewTask= task=>{
+  document.getElementById("tasks").appencChild(task);
+};
+}
+
+
+
+
+
+
+
+
+
+
+
+
 });
